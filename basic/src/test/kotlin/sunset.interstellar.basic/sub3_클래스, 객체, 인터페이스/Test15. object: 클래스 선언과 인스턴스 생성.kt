ba1f47c71d06021a92cc6dd588a2066dd51fdb0e
sunset-choe <>
2021-11-02 08:42:31 +0900

    .

--- basic/src/test/kotlin/sunset.interstellar.basic/sub3_클래스, 객체, 인터페이스/Test15. object: 클래스 선언과 인스턴스 생성.kt
+++ basic/src/test/kotlin/sunset.interstellar.basic/sub3_클래스, 객체, 인터페이스/Test15. object: 클래스 선언과 인스턴스 생성.kt
@@ -10,6 +10,15 @@ class Test15 {
         Payroll.allEmployees.add(Person("침착맨", true))
         Payroll.caculateSalary()
     }
+
+    @Test
+    fun `동반 객체`() {
+        A.bar()
+
+        val subscribingUser = User6.newSubscribingUser("bob@gmail.com")
+        val facebookUser = User6.newFacebookUser(4)
+        println(subscribingUser.nickname)
+    }
 }
 
 // 객체 선언: 싱글턴 객체
@@ -26,5 +35,40 @@ data class Person1(val name: String) {
     // 중첩 객체
     object NameComparator : Comparator<Person1> {
         override fun compare(p1: Person1, p2: Person1): Int = p1.name.compareTo(p2.name)
+    }
+}
+
+class A {
+    // A 클래스의 동반 객체. 자바의 정적 메소드 호출, 정적 필드 사용 구문과 같다
+    // 동반 객체는 자신을 둘러싼 클래스의 모든 private 멤버에 접근할 수 있다.
+    companion object {
+        fun bar() {
+            println("Companion object called")
+        }
+    }
+}
+
+// 부 생성자를 팩토리 메소드로 대신하기
+class User6 private constructor(val nickname: String) { // 주 생성자를 비공개로 만든다
+    companion object { // 동반 객체를 이용해 팩토리 메소드 정의
+        fun newSubscribingUser(email: String) =
+            User6(email.substringBefore('@'))
+
+        fun newFacebookUser(accountId: Int) =
+            User6(getFacebookName(accountId))
+    }
+}
+
+fun getFacebookName(accountId: Int): String {
+    // DB 조회...
+    return accountId.toString()
+}
+
+// 동반 객체에 이름 붙이기
+class PersonA(val name: String) {
+    companion object Loader {
+        fun fromJSON(jsonText: String): PersonA {
+            return PersonA("JSON")
+        }
     }
 }
